031e9c77b51bce8580a05f60594f5a346096a072
김동현 <@DESKTOP-OHFRQ5I>
2017-03-24 15:29:52 +0900

    실습과제 4-2

---
+++ JavaPractice/src/org/dimigo/oop/CarTest2.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *     |_  CarTest
+ *
+ * 1.  개요 :
+ * 2.  작성일 :  2017. 3. 23.
+ * <pre>
+ * 
+ * @author  :  김동현
+ * @version  : 1.0
+ */
+public class CarTest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Car2 car = new Car2("현대자동차","제네시스","검정색",225,50000000); 
+		System.out.println("<<자동차 목록>>");
+		System.out.printf("제조사명 : ");
+		System.out.println(car.getCompany());
+		System.out.printf("모델명 : ");
+		System.out.println(car.getModel());
+		System.out.printf("색상 : ");
+		System.out.println(car.getColor());
+		System.out.printf("최대속도 : ");
+		System.out.print(car.getMaxSpeed());
+		System.out.println("km");
+		System.out.printf("가격 : ");
+		System.out.printf(String.format("%,d",car.getPrice()));
+		System.out.println("원");
+		System.out.println();
+		Car2 car2 = new Car2("기아자동차","K7","흰색",246,40000000);
+		System.out.printf("제조사명 : ");
+		System.out.println(car2.getCompany());
+		System.out.printf("모델명 : ");
+		System.out.println(car2.getModel());
+		System.out.printf("색상 : ");
+		System.out.println(car2.getColor());
+		System.out.printf("최대속도 : ");
+		System.out.print(car2.getMaxSpeed());
+		System.out.println("km");
+		System.out.printf("가격 : ");
+		System.out.printf(String.format("%,d",car2.getPrice()));
+		System.out.println("원");
+		Car2 car3 = new Car2("삼성자동차","SM7","회색",200,38000000);
+		System.out.println();
+		System.out.printf("제조사명 : ");
+		System.out.println(car3.getCompany());
+		System.out.printf("모델명 : ");
+		System.out.println(car3.getModel());
+		System.out.printf("색상 : ");
+		System.out.println(car3.getColor());
+		System.out.printf("최대속도 : ");
+		System.out.print(car3.getMaxSpeed());
+		System.out.println("km");
+		System.out.printf("가격 : ");
+		System.out.printf(String.format("%,d",car3.getPrice()));
+		System.out.println("원");
+		
+		
+		
+
+	}
+
+}
