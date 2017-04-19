93cd1c018e121bf0e8e66169f80fafc2c38c26ad
김동현 <@DESKTOP-OHFRQ5I>
2017-04-19 12:41:45 +0900

    실습과제 8

---
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *     |_  PiggyBankTest
+ *
+ * 1.  개요 :
+ * 2.  작성일 :  2017. 4. 19.
+ * <pre>
+ * 
+ * @author  :  김동현
+ * @version  : 1.0
+ */
+public class PiggyBankTest {
+
+	
+	public static void main(String[] args) {
+		
+	FamilyMember dad=	new FamilyMember("아빠");
+	FamilyMember mom=	new FamilyMember("엄마");
+	FamilyMember me=	new FamilyMember("나");
+	FamilyMember brother=	new FamilyMember("남동생");
+	
+	FamilyMember.printMemberCnt();
+	PiggyBank.putMoney(dad,10000 );
+	PiggyBank.putMoney(mom,5000 );
+	PiggyBank.putMoney(me,2000);
+	PiggyBank.putMoney(brother,1000 );
+	PiggyBank.printBalance();
+	PiggyBank.putMoney(me,1000);
+	PiggyBank.printBalance();
+	}
+
+}
